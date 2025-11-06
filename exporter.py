c64694617b6130fa64c9132408dc7a5b86c26257
Mans0089 <manuxdl096@gmail.com>
2025-11-06 16:20:28 -0500

    add exporter.py change main.py/requerements.txt

---
+++ exporter.py
@@ -0,0 +1,43 @@
+from opentelemetry import trace, metrics
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+
+# Define service name
+resource = Resource(attributes={
+    SERVICE_NAME: "visemans"
+})
+
+# ----------- TRACES -----------
+provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(provider)
+
+otlp_span_exporter = OTLPSpanExporter(
+    endpoint="https://api.axiom.co/v1/traces",
+    headers={
+        "Authorization": "Bearer xaat-5fd464bf-e58d-44e0-80e8-f206f0f8b002",
+        "X-Axiom-Dataset": "viseapi"
+    }
+)
+provider.add_span_processor(BatchSpanProcessor(otlp_span_exporter))
+
+service_tracer = trace.get_tracer("viseapi")
+
+# ----------- METRICS -----------
+otlp_metric_exporter = OTLPMetricExporter(
+    endpoint="https://api.axiom.co/v1/metrics",
+    headers={
+        "Authorization": "Bearer xaat-5fd464bf-e58d-44e0-80e8-f206f0f8b002",
+        "X-Axiom-Dataset": "viseapi"
+    }
+)
+
+metric_reader = PeriodicExportingMetricReader(otlp_metric_exporter)
+meter_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+metrics.set_meter_provider(meter_provider)
+
+service_meter = metrics.get_meter("viseapi")
